7072812c7e11d9aa43d08a40ffc98cddae3efacc
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-07-04 17:36:57 +0530

    major

--- src/test/java/com/ecommerce/ecommerce/MajorApplicationTests.java
+++ src/test/java/com/ecommerce/ecommerce/MajorApplicationTests.java
@@ -1,13 +1,14 @@
-package com.sheryians.major;
+package com.ecommerce.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MajorApplicationTests {
+class EcommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
+
